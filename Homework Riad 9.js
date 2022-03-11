f2eaf807d8f56e296296cddc1d2c9943f4c91aef
Androniga <96742426+Androniga@users.noreply.github.com>
2022-03-11 11:48:31 +0100

    Add files via upload

---
+++ Homework Riad 9.js
@@ -0,0 +1,13 @@
+arr = [-3, 8, 7, 6, 5, -4, 3, 2, 1];
+var i, j, x;
+for (i = 0; i <= 9; i++) {
+  for (j = i + 1; j <= 9; j++) {
+    if (arr[i] > arr[j]) {
+      x = arr[i];
+      arr[i] = arr[j];
+      arr[j] = x;
+    }
+  }
+}
+console.log(arr);  //whoever reads this , thank you for your hard work teaching us and giving us your time. i appreciate it from the bottom from my heart <3 <3 <3 keep it up, and thank you thank you thank you.
+
